471015627691b1ca0a2b553647240d12e8d194bd
amarkos <amarkos@gmail.com>
2022-12-12 16:15:30 +0200

    updt

--- catdist/R/catdistNN.R
+++ catdist/R/catdistNN.R
@@ -1,14 +1,15 @@
-cat_nearest_neighbors<-function(train_df,assess_df = NULL, k, method = method){
+catdistNN <-function(train_df,assess_df = NULL, y = NULL, k, method = method){
   # source("R/cat_delta.r")
   # source("R/CalculateDistances2.R")
   # library(fastDummies)
-
+  colnames(train_df)[y] = "response"
+  colnames(assess_df)[y] = "response"
   train_df = train_df %>%  map_df(~fct_drop(.))
   train_resp = train_df %>% pull(response) %>% as.character
   train_df = train_df %>% select(-response)
   truth = assess_df %>% pull(response) %>% as.character
   assess_df = assess_df %>% select(-response)
-
+  
   prep_data = list()
   prep_data$df=train_df
   prep_data$Q<-as.numeric(lapply(train_df,nlevels))
@@ -17,8 +18,9 @@ cat_nearest_neighbors<-function(train_df,assess_df = NULL, k, method = method){
   # out_cdist = fct_delta(df=train_df, y=train_resp, method = method)
   # print(method)
   # delta = out_cdist$deltas[[method]] %>% as.matrix
-  delta = cat_delta(x = train_df, y = train_resp, method = method)[[1]]
-  
+ 
+  delta = cat_delta(x = train_df, y = train_resp, method = method)[[2]]
+ 
   Z_tr = dummy_cols(train_df, remove_selected_columns = TRUE) %>%
     as_tibble()
   # out_delta = cat_delta(x=train_df,y=NULL,method = method)
@@ -29,8 +31,9 @@ cat_nearest_neighbors<-function(train_df,assess_df = NULL, k, method = method){
 
   Z_ts = dummy_cols(rbind(assess_df), remove_selected_columns = TRUE) %>%
     as_tibble() %>% select(names(Z_tr))
-  #print(dim(Z_tr))
-  #print(dim(delta))
+#  print(dim(Z_tr))
+ # print(dim(delta))
+  
   D_tr_ts = (Z_tr %>% data.matrix()) %*% delta %*% t(Z_ts%>% data.matrix())
   D_tr_ts = as.data.table(D_tr_ts)
   preds = map_chr(.x = D_tr_ts, .f= function(x = .x){
